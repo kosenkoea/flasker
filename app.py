ac97f41ad5aeede0f956dce5cb0f83994059907c
Evgenii Kosenko <kosenkoea@bk.ru>
2025-01-05 23:12:57 +0500

    update many files

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     users = ['evgenii','ivan','yura','irina']
     return render_template('index.html',users=users)
 
 @app.route('/user/<name>')
-def about(name):
+def user(name):
     return render_template('user.html', user_name = name)
 
 # Error 404. Page not found.
